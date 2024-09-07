eb708b2901b82c240d005730df0eadde7533c3f4
Oleksandr Zhylenko <mainglot@gmail.com>
2024-09-07 16:52:43 +0300

    Add Docker support

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,15 +5,20 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { TodosModule } from './todos/todos.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     AuthModule,
-    MongooseModule.forRoot('mongodb://localhost/nest', {}),
+    MongooseModule.forRoot(process.env.MONGODB_PATH, {}),
     UsersModule,
     TodosModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'frontend', 'dist'),
+    }),
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
